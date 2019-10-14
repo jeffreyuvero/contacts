ff91ab7fccd35d9baec0b689581b23f38a439aa7
Jeffrey E. Uvero <jeffrey.uvero@amellar.com>
2019-10-14 13:49:56 +0800

    Initialize App and edit app.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Contacts Management</h1>
+        <button>ADD Contact</button>
+        <hr />
+        <p>name || contact number</p>
+        <p>name || contact number</p>
+        <p>name || contact number</p>
+    </div>
+  );
+}
+
+export default App;
